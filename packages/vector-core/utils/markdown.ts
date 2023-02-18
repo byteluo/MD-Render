fb27224aeaafaf34fb60af37a04cf731d327c703
treecat <2184452844@qq.com>
2023-02-18 22:06:16 +0800

    refact: 改良 markdown

--- packages/vector-core/utils/markdown.ts
+++ packages/vector-core/utils/markdown.ts
@@ -6,50 +6,69 @@ import { renderImage } from "./image";
 
 const md = MarkdownIt({ html: true });
 
-function getMergeProps(
+function getValueFromObjects(
   key: string,
-  obj1: any = {},
-  obj2: any = {},
-  defaultValue?: any
-) {
-  // todo
-  return {} as any;
+  obj1: Record<string, any> = {},
+  obj2: Record<string, any> = {},
+  defaultValue: any
+): any {
+  if (key in obj1) {
+    return obj1[key];
+  } else if (key in obj2) {
+    return obj2[key];
+  } else {
+    return defaultValue;
+  }
 }
 
-export async function renderMarkdownFile(filePath: string): Promise<HookObj> {
-  const fileDescriptor = getFileReader(filePath).getFileDescriptor();
-  if (!fileDescriptor.isReady()) {
-    await fileDescriptor.ready();
-  }
+export async function renderMarkdownFile(
+  filePath: string
+): Promise<HookObj | null> {
+  try {
+    const fileDescriptor = getFileReader(filePath).getFileDescriptor();
+    if (!fileDescriptor.isReady()) {
+      await fileDescriptor.ready();
+    }
 
-  // renderImage
-  fileDescriptor.content = await renderImage(
-    filePath,
-    fileDescriptor.content,
-    "markdown"
-  );
-
-  const rawContent = fileDescriptor.content;
-  let { attributes, body: markdownBody } = fm(rawContent) as any;
-
-  const id = getMergeProps("id", attributes, fileDescriptor.md5);
-  const title = getMergeProps("title", attributes, fileDescriptor);
-  const ctime = getMergeProps("ctime", attributes, fileDescriptor, 0);
-  const mtime = getMergeProps("mtime", attributes, fileDescriptor, 0);
-
-  return {
-    id,
-    md5: fileDescriptor.md5,
-    ctime,
-    title,
-    mtime,
-    word: markdownBody.length,
-    content: md.render(rawContent),
-    _private: {
+    // renderImage
+    fileDescriptor.content = await renderImage(
       filePath,
-      except: false,
-      rawContent,
-      markdownBody,
-    },
-  };
+      fileDescriptor.content,
+      "markdown"
+    );
+
+    const rawContent = fileDescriptor.content;
+    let { attributes, body: markdownBody } = fm(rawContent);
+
+    const [id, title, ctime, mtime] = [
+      ["title", ""],
+      ["ctime", 0],
+      ["mtime", 0],
+    ].map(([key, defaultValue]) =>
+      getValueFromObjects(
+        key as any,
+        attributes as any,
+        fileDescriptor,
+        defaultValue
+      )
+    );
+
+    return {
+      id,
+      md5: fileDescriptor.md5,
+      ctime,
+      title,
+      mtime,
+      word: markdownBody.length,
+      content: md.render(rawContent),
+      _private: {
+        filePath,
+        except: false,
+        rawContent,
+        markdownBody,
+      },
+    };
+  } catch (error) {
+    return null;
+  }
 }
