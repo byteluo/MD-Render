7702fd204880adad6208c827028bd367b804ebe5
Treecat <treecat@fanruan.com>
2023-03-23 13:49:00 +0800

    fix: 修复 CRUD

--- packages/theme-nana/src/store/crud.js
+++ packages/theme-nana/src/store/crud.js
@@ -1,60 +1,62 @@
 import { sendStaticGetRequest } from "@/store/api";
 import { isDevMode } from "@/service/dev";
 export {
-  getAboutInfo,
-  getPostList,
-  getPostDetail,
-  getThinkingData,
+    getAboutInfo,
+    getPostList,
+    getPostDetail,
+    getThinkingData
 } from "vector-core";
 
 export function useData(data, fetch) {
-  const loaded = ref(false);
-  let isDesctoryed = false;
-  const invokeQueue = [];
-  const stopWatch = watch(
-    loaded,
-    (v) => {
-      if (v) {
-        invokeQueue.forEach((cb) => {
-          cb();
-        });
-        invokeQueue.length = 0;
+    const loaded = ref(false);
+    let isDesctoryed = false;
+    const invokeQueue = [];
+    const stopWatch = watch(
+        loaded,
+        (v) => {
+            if (v) {
+                invokeQueue.forEach((cb) => {
+                    cb();
+                });
+                invokeQueue.length = 0;
+                stopWatch();
+            }
+        },
+        { immediate: true }
+    );
+    const getData = async () => {
+        const res = await fetch();
+        loaded.value = true;
+        Object.assign(data, res);
+        if (isDevMode()) {
+            getData();
+        }
+    };
+    getData();
+    const onDataLoaded = (cb) => {
+        if (loaded.value) {
+            cb();
+        } else {
+            invokeQueue.push(cb);
+        }
+    };
+    const destory = () => {
         stopWatch();
-      }
-    },
-    { immediate: true }
-  );
-  const getData = async () => {
-    const res = await fetch();
-    loaded.value = true;
-    Object.assign(data, res);
-    if (isDevMode()) {
-      getData();
-    }
-  };
-  getData();
-  const onDataLoaded = (cb) => {
-    if (loaded.value) {
-      cb();
-    } else {
-      invokeQueue.push(cb);
-    }
-  };
-  const destory = () => {
-    stopWatch();
-    isDesctoryed = true;
-  };
-  return {
-    onDataLoaded,
-    loaded,
-    destory,
-  };
+        isDesctoryed = true;
+    };
+    return {
+        onDataLoaded,
+        loaded,
+        destory
+    };
 }
 
 export async function getModuleData(module) {
-  return sendStaticGetRequest(`/${module}.json`);
+    return sendStaticGetRequest(`/${module}.json`);
 }
 
 export async function getPostListMap() {
-  return (await sendStaticGetRequest(`/post/list.map.json`)).map;
+    return (await sendStaticGetRequest(`/post/list.map.json`)).map;
 }
+
+export function usePostDetail() {}
